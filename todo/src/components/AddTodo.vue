ab1a8548849aae0aded79d5275e676cb41338e25
Govind Murthi <govindmurthi@Govinds-MacBook-Pro.local>
2020-03-04 06:06:42 -0500

    add todos

---
+++ todo/src/components/AddTodo.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="modal-container"  v-bind:class="{ 'show-modal': open }">
+    <div class="modal">
+      <div class="modal-header">
+        <button class="modal-close" @click="$emit('hideAddDialog')">X</button>
+      </div>
+      <form class="add-todo-form" @submit="addTodo">
+        <input type="text" v-model="text" name="text" id="" />
+        <input type="submit" value="Add Todo" class="btn" />
+      </form>
+    </div>
+    <div class="modal-background"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "AddTodo",
+  props: ["open"],
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {
+      addTodo(e) {
+         this.$emit('addTodo', this.text);
+         e.stopPropagation();
+         e.preventDefault();
+         return false;
+      }
+  }
+};
+</script>
+<style scoped>
+.modal-container {
+    display: none;
+}
+
+.modal {
+    position: absolute;
+    z-index: 10000;
+    border: 1px solid #000;
+    padding: 20px;
+    width: 50%;
+    top: 30%;
+    margin: 0px auto;
+    background: #fff;
+    left: 25%;
+}
+
+.modal-header {
+  height: 30px;
+  background: #e4e4e4;
+  font-size: 4px;
+  text-align: end;
+}
+
+.modal-background {
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: #000;
+  z-index: 90;
+  opacity: 0.5;
+  width: 100%;
+  height: 1000px;
+}
+
+.show-modal {
+  display: block;
+}
+</style>
--- todo/src/components/Todos.vue
+++ todo/src/components/Todos.vue
@@ -1,36 +1,61 @@
 <template>
-<div id="todo-container">
-  <div v-for="todo in todos" :key="todo.id">
-    <TodoItem v-bind:todo="todo" @deleteTodo="deleteTodo"/>
-  </div></div>
+  <div id="todo-container">
+    <div v-for="todo in todos" :key="todo.id">
+      <TodoItem v-bind:todo="todo" @deleteTodo="deleteTodo" />
+    </div>
+    <div class="add-todo-container">
+        <button class="add-todo" @click="showAddDialog">Add To Do</button>
+    </div>
+    <AddTodo v-bind:open="isAddDialogVisible" @hideAddDialog="hideAddDialog" @addTodo="addTodo" />
+  </div>
 </template>
 
 <script>
-import TodoItem from './TodoItem.vue';
+import TodoItem from "./TodoItem.vue";
+import AddTodo from "./AddTodo.vue";
 export default {
   name: "Todos",
   components: {
-      TodoItem
+    TodoItem,
+    AddTodo
   },
   data() {
     return {
       todos: [
-          { id: 1, text: "Todo 1", isChecked: true },
-          { id: 2, text: "Todo 2", isChecked: false }
-        ]
+        { id: 1, text: "Todo 1", isChecked: true },
+        { id: 2, text: "Todo 2", isChecked: false }
+      ],
+      isAddDialogVisible: false
     };
   },
   methods: {
-      deleteTodo(id) {
-          this.todos = this.todos.filter(t => t.id !== id);
-      }
+    deleteTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id);
+    },
+    showAddDialog() {
+        this.isAddDialogVisible = true;
+    },
+    hideAddDialog() {
+        this.isAddDialogVisible = false;
+    },
+    addTodo(text) {
+        let newId = 1;
+        const lastTodo = this.todos[this.todos.length - 1];
+        newId = lastTodo ? lastTodo.id + 1 : newId;
+
+        this.todos = [...this.todos, {
+            id: newId, text: text, isChecked: false
+        }];
+
+        this.hideAddDialog();
+    }
   }
 };
 </script>
 <style scoped>
 #todo-container {
-    padding: 20px;
-    margin: 0px auto;
-    border: 1px solid #000;
+  padding: 20px;
+  margin: 0px auto;
+  border: 1px solid #000;
 }
 </style>
